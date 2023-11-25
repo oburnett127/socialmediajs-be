95f945751890e2dcd8a60d80d3ddfa528f8938d3
dlutton <eyedea32@gmail.com>
2023-11-24 23:51:44 -0800

    adding inversify

--- app/server.ts
+++ app/server.ts
@@ -1,37 +1,38 @@
-import express, { Application } from "express";
-import cors from "cors";
-import db from "./models/index.js";
+import 'reflect-metadata';
+import { InversifyExpressServer } from 'inversify-express-utils';
+import { Container } from 'inversify';
+import * as express from 'express';
+import { JobService } from './service/job';
+import db from './models/index.js';
+import { TYPES } from './service/types';
 
-const app: Application = express();
+import './controllers/job';
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-};
+// load everything needed to the Container
+let container = new Container();
+container.bind<JobService>(TYPES.JobService).to(JobService);
 
-app.use(cors(corsOptions));
+// start the server
+let server = new InversifyExpressServer(container);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+server.setConfig((app) => {
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+});
 
 db.sequelize.sync()
   .then(() => {
-    console.log("Synced db.");
+    console.log('Synced db.');
   })
   .catch((err: Error) => {
-    console.log("Failed to sync db: " + err.message);
+    console.log('Failed to sync db: ' + err.message);
   });
 
-import jobRoutes from "./routes/job.routes.js";
-import stakeholderRoutes from "./routes/stakeholder.routes.js";
-import userinfoRoutes from "./routes/userinfo.routes.js";
-
-jobRoutes(app);
-stakeholderRoutes(app);
-userinfoRoutes(app);
+const serverInstance = server.build();
 
 const portString: string | undefined = process.env.PORT;
-const PORT: number = parseInt(portString || '8080', 10);
+const port: number = parseInt(portString || '8080', 10);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+serverInstance.listen(port);
+
+console.log(`Server started on port ${port} :)`);
